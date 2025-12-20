2abd6d8b658414f571d8f5de61d9e507dc1b41b0
Jack O'Connor <oconnor663@gmail.com>
2025-12-20 15:38:01 -0800

    .await in the bodies is a compiler error

---
+++ tests/ui/await_in_body.rs
@@ -0,0 +1,18 @@
+#![deny(warnings)]
+
+use futures::stream;
+use join_me_maybe::join_me_maybe;
+use std::future::ready;
+
+#[tokio::main]
+async fn main() {
+    let my_stream = stream::iter([3, 4, 5]);
+    join_me_maybe!(
+        _ = ready(1) => {
+            ready(2).await
+        }
+        _ in stream::iter([3, 4, 5]) => {
+            ready(6).await;
+        }
+    );
+}
